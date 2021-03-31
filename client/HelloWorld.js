1d45016338a01d779fb1239c0856c3d480d6ecb8
zerontec <zerontecnologia1@gmail.com>
2021-03-30 23:01:55 -0400

    Initial Config, create server and client folder, Config MERN

---
+++ client/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const HelloWorld = () => {
+    return (
+        <div>
+          <h1>Hello World and Universe!</h1>
+        </div>
+      )
+}
+
+export default hot(module)(HelloWorld)
